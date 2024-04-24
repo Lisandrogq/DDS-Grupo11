90491535d9dd6f1bc72e34de19817b35ac0e2aa3
sofitofischer <mrabeyfischer@frba.utn.edu.ar>
2024-04-24 11:20:25 -0300

    construcor de tramo

--- assignments/mail/src/main/java/org/grupo11/services/Shipping/Tramo.java
+++ assignments/mail/src/main/java/org/grupo11/services/Shipping/Tramo.java
@@ -7,5 +7,10 @@ public class Tramo {
     private Stop destination;
     private Postman postman;
     
+    public Tramo(Stop origin, Stop destination, Postman postman) {
+        this.origin = origin;
+        this.destination = destination;
+        this.postman = postman;
+    }
 
 }
