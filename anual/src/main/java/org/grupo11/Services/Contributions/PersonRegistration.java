4a52927b37e84d5153439094a2083aff24a03a66
nicolau <marcosnicolau2003@gmail.com>
2024-05-26 12:06:53 -0300

    anual: person registration contribution

--- anual/src/main/java/org/grupo11/Services/Contributions/PersonRegistration.java
+++ anual/src/main/java/org/grupo11/Services/Contributions/PersonRegistration.java
@@ -1,5 +1,21 @@
 package org.grupo11.Services.Contributions;
 
-public class PersonRegistration {
+import org.grupo11.Services.PersonInNeed;
+import org.grupo11.Services.Contributor.Contributor;
 
+public class PersonRegistration extends Contribution {
+    PersonInNeed person;
+
+    public PersonRegistration(PersonInNeed person) {
+        this.person = person;
+    }
+
+    @Override
+    protected boolean validate(Contributor contributor) {
+        return (super.validate(contributor) && contributor.address != null);
+    }
+
+    public ContributionType getContributionType() {
+        return ContributionType.PERSON_REGISTRATION;
+    }
 }
