8e26e11aecf7a33ba0d4014b9373ad632bd281ca
nicolau <marcosnicolau2003@gmail.com>
2024-06-15 12:05:27 -0300

    fix(anual): fridge allocator request

--- anual/src/main/java/org/grupo11/Services/Fridge/FridgeAllocatorRes.java
+++ anual/src/main/java/org/grupo11/Services/Fridge/FridgeAllocatorRes.java
@@ -2,11 +2,13 @@ package org.grupo11.Services.Fridge;
 
 import java.util.List;
 
-public class FridgeAlLocatorRes {
-    static class Location {
+public class FridgeAllocatorRes {
+    public static class Location {
         public float lon;
         public float lat;
     }
 
+    public String id;
+    public long createdAt;
     public List<Location> locations;
 }
