5ffd492568b1c241c52266a835e7cc76532ba2bd
sofitofischer <mrabeyfischer@frba.utn.edu.ar>
2024-09-01 16:09:43 -0300

    login y signup entrega4

--- anual/frontend/ejemplo/js/script.js
+++
@@ -1,8 +0,0 @@
-function cambiarColor() {
-	let color = document.getElementById("color-selector").value;
-	let element = document.getElementById("cuadrado2");
-	
-	element.classList.remove("azul");
-	element.classList.remove("rojo");
-	element.classList.add(color);
-}
