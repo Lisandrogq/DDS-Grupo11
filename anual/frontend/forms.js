937394d42c43440efb7815bdaf473b73567e9ee8
sofitofischer <mrabeyfischer@frba.utn.edu.ar>
2024-09-01 16:09:19 -0300

    login y signup entrega4

--- anual/frontend/forms.js
+++ anual/frontend/forms.js
@@ -1,10 +1,14 @@
-function actualizarConNombre() {
-	let mail = document.getElementById("contrasena").value;
+function validar() {
+	let mail = document.getElementById("mail").value;
 	let contrasena= document.getElementById("contrasena").value;
+    let contrasenaConfirmada = document.getElementById("contrasenaConfirmada").value;
+
     if(mail==""||contrasena=="")
     alert("Completar todos los campos");
 
     if(mail == "admin"||contrasena=="admin"||contrasena=="1234") 
     alert("La contraseña ingresada no está permitida");
     
+    if(contrasena!=contrasenaConfirmada)
+    alert("Las contraseñas no coinciden");
 }
