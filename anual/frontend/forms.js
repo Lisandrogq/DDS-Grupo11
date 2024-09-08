66c9435bd6f4a1484383c85822a5e137b6c24fc1
Val <vvalmadreamoia@frba.utn.edu.ar>
2024-09-08 13:05:01 -0300

    meal distribution

--- anual/frontend/forms.js
+++ anual/frontend/forms.js
@@ -12,3 +12,34 @@ function validar() {
     if(contrasena!=contrasenaConfirmada)
     alert("Las contraseñas no coinciden");
 }
+
+window.onload = function() {
+
+	let elementoBtnEliminar = document.getElementById("btnEliminarInput");
+	let elementoBtnCrear = document.getElementById("btnCrearInput");
+
+	//Agrega evento click al elemento Botón Eliminar
+	elementoBtnEliminar.addEventListener("click", function() {
+		eliminarInput();
+	});
+
+	//Agrega evento click al elemento Botón Crear
+	elementoBtnCrear.addEventListener("click", function() {
+		agregarInput();
+	});
+
+};
+
+function eliminarInput() {
+	var inputs = document
+		.getElementById("input-placeholder")
+		.querySelectorAll(".input");
+		inputs[inputs.length-1].remove();
+}
+
+function agregarInput() {
+	let div = document.createElement("div");
+	div.classList.add("input");
+	div.innerHTML = ' <input type="text" id="meal" name="meal" required placeholder="ID of meal to distribute..."class="col-12 inputs">';
+	document.getElementById("input-placeholder").appendChild(div);
+}
