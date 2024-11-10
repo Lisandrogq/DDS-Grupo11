07cd4e6ea1cdb3903d7aa6569d051b83723bbe04
nicolau <marcosnicolau2003@gmail.com>
2024-11-10 14:08:50 -0300

    feat(infra): get certficiates from ansible

--- anual/infra/scripts/deploy.sh
+++ anual/infra/scripts/deploy.sh
@@ -2,17 +2,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CONFIG_FILE=$SCRIPT_DIR/deploy-conf.json
 
 ANSIBLE_HOST=`jq -r ".host_ansible" $CONFIG_FILE`
-HOST=`jq -r ".host" $CONFIG_FILE`
 USER=`jq -r ".user" $CONFIG_FILE`
 
 echo "\n========== Starting deployment =========="
-
 ansible-playbook -u $USER "$SCRIPT_DIR/../ansible/server.yaml" -e "@$CONFIG_FILE" --limit $ANSIBLE_HOST
-
-if [ $? -eq 0 ]; then
-    "$SCRIPT_DIR/certificate.sh" "$USER" "$HOST"
-else
-    echo "Ansible playbook failed. Skipping certificate script."
-fi
-
 echo "\n========== Server deployment finished =========="
