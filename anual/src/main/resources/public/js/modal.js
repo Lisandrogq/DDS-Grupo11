1d76e60475f444b4b94046716493f5c3573a94e5
Lisandro Gonzalez <lisandrogq7@gmail.com>
2024-12-06 00:02:23 -0300

    feat:person registration

--- anual/src/main/resources/public/js/modal.js
+++ anual/src/main/resources/public/js/modal.js
@@ -435,53 +435,33 @@ function personRegistration() {
 			</div>
 
 			<form method="POST" action="/contribution/registration" class="form">
+				
 				<input
 					type="text"
-					id="username"
-					name="username"
-					required
-					placeholder="Username..."
-				/>
-				<input
-					type="password"
-					id="contrasena"
-					name="contrasena"
-					required
-					placeholder="Password..."
-				/>
-				<input
-					type="password"
-					id="contrasenaConfirmada"
-					name="contrasena"
-					required
-					placeholder="Password again..."
-				/>
-				<input
-					type="text"
-					id="nombreYApellido"
-					name="nombreYApellido"
+					id="name"
+					name="name"
 					required
 					placeholder="Full name..."
+				/>
+				<input
+					type="number"
+					id="dni"
+					name="dni"
+					required
+					placeholder="DNI..."
 				/>
 				<p>Birthdate</p>
 				<input
 					type="date"
-					id="fechaDeNacimiento"
-					name="fechaDeNacimiento"
+					id="birth"
+					name="birth"
 					required
 					placeholder="Birthdate..."
 				/>
 				<input
-					type="text"
-					id="idtarjeta"
-					name="idtarjeta"
-					required
-					placeholder="Card ID..."
-				/>
-				<input
 					type="number"
-					id="childrenInCharge"
-					name="childrenInCharge"
+					id="children_count"
+					name="children_count"
 					required
 					min=0
 					placeholder="Number of children in charge..."
