724785770555759f85576dc3d0164ebac70ca27e
nicolau <marcosnicolau2003@gmail.com>
2024-12-12 13:00:44 -0300

    feat: trick to use custom btn

--- anual/src/main/resources/public/js/social-connection.js
+++ anual/src/main/resources/public/js/social-connection.js
@@ -1,15 +1,20 @@
 const googleBtn = document.querySelector("#google-connect-btn");
 const githubBtn = document.querySelector("#github-connect-btn");
 
-googleBtn.onclick = () => {};
-
 const startApp = () => {
 	window.onGoogleLibraryLoad = () => {
 		google.accounts.id.initialize({
 			client_id: "933613917422-bb9a9ptfqof8saqbc5ub1gr5mtpoohh2.apps.googleusercontent.com",
 			callback: handleResponse,
 		});
-		google.accounts.id.prompt();
+		const parent = document.querySelector("#google-btn-wrapper");
+		google.accounts.id.renderButton(parent, {
+			type: "icon",
+		});
+		const googleLoginWrapperButton = parent.querySelector("div[role=button]");
+		window.handleGoogleConnection = () => {
+			googleLoginWrapperButton.click();
+		};
 	};
 };
 
@@ -29,20 +34,6 @@ const handleResponse = async (googleUser) => {
 	// if (res?.status != 200) {
 	// 	alert(`Could not add login provider: ${res?.message}`);
 	// }
-};
-
-const attachSignin = (element) => {
-	auth2.attachClickHandler(
-		element,
-		{},
-		async (googleUser) => {
-			alert("Google account connected!");
-		},
-		(error) => {
-			console.log("error", error);
-			alert(`Could not add login provider: ${error}`);
-		}
-	);
 };
 
 startApp();
