70f85db3c15091eaedf7fd6ef6307436cd4a12c2
nicolau <marcosnicolau2003@gmail.com>
2024-12-12 12:30:48 -0300

    feat: migrate to new library

--- anual/src/main/resources/public/js/social-connection.js
+++ anual/src/main/resources/public/js/social-connection.js
@@ -3,15 +3,32 @@ const githubBtn = document.querySelector("#github-connect-btn");
 
 googleBtn.onclick = () => {};
 
-var googleUser = {};
 const startApp = () => {
-	gapi.load("auth2", function () {
-		auth2 = gapi.auth2.init({
+	window.onGoogleLibraryLoad = () => {
+		google.accounts.id.initialize({
 			client_id: "933613917422-bb9a9ptfqof8saqbc5ub1gr5mtpoohh2.apps.googleusercontent.com",
-			cookiepolicy: "single_host_origin",
+			callback: handleResponse,
 		});
-		attachSignin(document.getElementById("#google-connect-btn"));
-	});
+		google.accounts.id.prompt();
+	};
+};
+
+const handleResponse = async (googleUser) => {
+	console.log("GOOGLE USER", googleUser);
+	// const id_token = googleUser.getAuthResponse().id_token;
+	// const req = await fetch("/user/provider/", {
+	// 	method: "POST",
+	// 	headers: {
+	// 		Accept: "application/json",
+	// 		"Content-Type": "application/json",
+	// 	},
+	// 	body: JSON.stringify({ id_token, provider: "google" }),
+	// });
+	// const res = await req.json();
+
+	// if (res?.status != 200) {
+	// 	alert(`Could not add login provider: ${res?.message}`);
+	// }
 };
 
 const attachSignin = (element) => {
@@ -19,23 +36,10 @@ const attachSignin = (element) => {
 		element,
 		{},
 		async (googleUser) => {
-			const id_token = googleUser.getAuthResponse().id_token;
-			const req = await fetch("/user/provider/", {
-				method: "POST",
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ id_token, provider: "google" }),
-			});
-			const res = await req.json();
-
-			if (res?.status != 200) {
-				alert(`Could not add login provider: ${res?.message}`);
-			}
 			alert("Google account connected!");
 		},
 		(error) => {
+			console.log("error", error);
 			alert(`Could not add login provider: ${error}`);
 		}
 	);
