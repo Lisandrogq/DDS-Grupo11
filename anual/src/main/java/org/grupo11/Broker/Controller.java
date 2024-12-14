8277a84f040cb8dc19ac2374269a066d172e634b
nicolau <marcosnicolau2003@gmail.com>
2024-12-14 16:33:00 -0300

    refactor: rabbit queue names

--- anual/src/main/java/org/grupo11/Broker/Controller.java
+++ anual/src/main/java/org/grupo11/Broker/Controller.java
@@ -16,12 +16,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Delivery;
 
 public class Controller {
-
     public Controller() {
-
     }
 
-    public static void handle_temp_update(String consumerTag, Delivery message) throws IOException {
+    public static void handleTempUpdate(String consumerTag, Delivery message) throws IOException {
         try {
             System.out.println("handle_temp_update");
             String json = new String(message.getBody(), "UTF-8");
@@ -29,6 +27,7 @@ public class Controller {
 
             FridgeTempDTO dto = JSON.parse(json, new TypeReference<FridgeTempDTO>() {
             });
+            // todo query fridge from db
             Fridge fridge = FridgesManager.getInstance().getById(dto.fridge_id);
             TemperatureSensor sensor = (TemperatureSensor) fridge.getTempManager().getSensorById(dto.sensor_id);
             sensor.setData(dto.temp);
@@ -37,18 +36,13 @@ public class Controller {
         }
     }
 
-    public static void handle_alert(String consumerTag, Delivery message) throws IOException {
+    public static void handleMovementDetected(String consumerTag, Delivery message) throws IOException {
         try {
-            System.out.println("handle_alert");
             String json = new String(message.getBody(), "UTF-8");
-            System.out.println(json);
-
             AlertDTO dto = new ObjectMapper().readValue(json, AlertDTO.class);
-
             Fridge fridge = FridgesManager.getInstance().getById(dto.fridge_id);
             Alert alert = new Alert(dto.type, dto.detectedAt);
             fridge.addIncident(alert);
-
         } catch (Exception e) {
             System.err.println("Err while handling an alert: " + e);
         }
